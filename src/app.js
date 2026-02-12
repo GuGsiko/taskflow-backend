d567a6a4e3259429bec7f0a22b97cfa50448f0e1
GuGsiko <vasiko162@gmail.com>
2026-02-12 12:48:28 +0400

    Fix app initialization order and debug route

--- src/app.js
+++ src/app.js
@@ -1,24 +1,25 @@
 import express from "express";
-import taskRoutes from "./routes/task.routes.js";
 import cors from "cors";
+import taskRoutes from "./routes/task.routes.js";
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// debug endpoint (აქ არის სწორი ადგილი)
 app.get("/__debug", (req, res) => {
   res.json({ alive: true });
 });
 
+// routes
+app.use("/api/tasks", taskRoutes);
 
-// const app = express();
-
-// app.use(cors());
-// // JSON body middleware 
-// app.use(express.json());
-
-// // routes
-// app.use("/api/tasks", taskRoutes);
-
-// // error handler 
-// app.use((err, req, res, next) => {
-//   console.error(err);
-//   res.status(500).json({ message: "Internal Server Error" });
-// });
+// error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: "Internal Server Error" });
+});
 
 export default app;
