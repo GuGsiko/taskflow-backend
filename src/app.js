4049bfed153827ad5bbeb8e265086338635ab6b5
GuGsiko <vasiko162@gmail.com>
2026-02-12 11:38:13 +0400

    Debugg back

--- src/app.js
+++ src/app.js
@@ -1,21 +1,24 @@
 import express from "express";
 import taskRoutes from "./routes/task.routes.js";
 import cors from "cors";
-
-
-const app = express();
-
-app.use(cors());
-// JSON body middleware 
-app.use(express.json());
-
-// routes
-app.use("/api/tasks", taskRoutes);
-
-// error handler 
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ message: "Internal Server Error" });
+app.get("/__debug", (req, res) => {
+  res.json({ alive: true });
 });
+
+
+// const app = express();
+
+// app.use(cors());
+// // JSON body middleware 
+// app.use(express.json());
+
+// // routes
+// app.use("/api/tasks", taskRoutes);
+
+// // error handler 
+// app.use((err, req, res, next) => {
+//   console.error(err);
+//   res.status(500).json({ message: "Internal Server Error" });
+// });
 
 export default app;
