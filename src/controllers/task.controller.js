c220e7238ae8c2584003834ba488adcd90cf6bd2
GuGsiko <vasiko162@gmail.com>
2026-02-14 20:36:26 +0400

    Stabilize backend task API with todo/done status contract

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,49 +1,46 @@
-import { createTask, fetchTasks } from "../services/task.service.js"; 
-import { toggleTaskStatus } from "../services/task.service.js";
-import { removeTask } from "../services/task.service.js";
+import {fetchTasks,createTask,toggleTaskStatus,removeTask,} from "../services/task.service.js";
 
-export const getAllTasks = async (req , res , next ) => {
-    try {
-        const tasks = fetchTasks ();
-        res.status(200).json(tasks);
-    }catch (err){
-        next(err);
-    }
-};
-
-export const addTask = async (req , res , next) => {
-    try {
-        const { title } = req.body;
-
-        if (!title){
-            return res.status(400).json({ message: "Title is required"});
-        }
-
-        const newTask = createTask(title);
-        res.status(201).json(newTask);
-    }   catch (err){
-        next (err);
-    }
-};
-
-export const toggleTask = async (req, res, next) => {
+export const getAllTasks = async (req, res, next) => {
   try {
-    const updated = await toggleTaskStatus(req.params.id);
-
-    if (!updated) {
-      return res.status(404).json({ message: "Task not found" });
-    }
-
-    res.json(updated);
+    const tasks = await fetchTasks();
+    res.status(200).json(Array.isArray(tasks) ? tasks : []);
   } catch (err) {
     next(err);
   }
 };
 
-export const deleteTask = (req, res, next) => {
+export const addTask = async (req, res, next) => {
   try {
-    const id = Number(req.params.id);
-    removeTask(id);
+    const { title } = req.body;
+
+    if (!title || !title.trim()) {
+      return res.status(400).json({ message: "Title is required" });
+    }
+
+    const task = await createTask(title);
+    res.status(201).json(task);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const toggleTask = async (req, res, next) => {
+  try {
+    const task = await toggleTaskStatus(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.status(200).json(task);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteTask = async (req, res, next) => {
+  try {
+    await removeTask(req.params.id);
     res.status(204).end();
   } catch (err) {
     next(err);
