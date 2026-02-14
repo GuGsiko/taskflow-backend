d1d5997431d6b9291783ff44d4d3d68a0209a989
GuGsiko <vasiko162@gmail.com>
2026-02-14 19:04:09 +0400

    Add MongoDB connection and Task model

---
+++ src/models/Task.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["todo", "done"],
+      default: "todo",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Task", taskSchema);
