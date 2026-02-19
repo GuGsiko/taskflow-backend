d448d4131e5f12ff03576394b70901b0252e1ec9
GuGsiko <vasiko162@gmail.com>
2026-02-20 02:22:10 +0400

    FIX auth controller bugs and JWT response

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,46 +2,72 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
-export const register = async (req, resizeBy, next ) => {
-    try{
-        const {email, password} = req.body;
+export const register = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-        if (!email || !password)
-            return resizeBy.status(400).json({ message: "Email and password required "});
-
-        const existing = await User.findOne({ email });
-        if (existing)
-            return resizeBy.status(400),json({ message: "User already exists "});
-
-        const hashed = await bcrypt.hash(password, 10);
-
-        const user = await User.create({
-            email,
-            password : hashed,
-        });
-
-        resizeBy.status(201).json({ message: "User created"});
-    }catch (err){
-        next (err);
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password required",
+      });
     }
+
+    const existing = await User.findOne({ email });
+    if (existing) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    await User.create({
+      email,
+      password: hashed,
+    });
+
+    res.status(201).json({ message: "User created" });
+
+  } catch (err) {
+    next(err);
+  }
 };
 
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-export const login = async (req , res , next ) => {
-    try {
-        const {email, passowrd} = req.body;
-
-        const user = await User.findOne({email});
-        if(!user)
-            return res.status(400).json({message :"Invaild credentails"});
-
-        const token = jwt.sign(
-            {userId: user._id},
-            process.env.JWT_SECRET,
-            {expiresIn:"7d"},
-        );
-        res.json9[token];
-    }catch(err){
-        next(err);
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password required",
+      });
     }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({ token });
+
+  } catch (err) {
+    next(err);
+  }
 };
